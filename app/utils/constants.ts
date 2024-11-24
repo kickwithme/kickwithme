9a504e57a16a064a7f3c0925db27551ad9e610f6
Sourblock <51027669+Sourblock@users.noreply.github.com>
2024-11-24 15:18:14 -0500

    Saved local changes before pulling

--- app/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export const classColors = {
-  dragons: 'text-[#954F5A]',
-  karateKids: 'text-[#4F7395]',
-  adults: 'text-[#4F9563]',
-  blackBelt: 'text-black',
-  demo: 'text-[#954F5A]',
-  leadership: 'text-[#954F5A]',
-};
-
--- app/utils/defaultFocuses.ts
+++
@@ -1,8 +0,0 @@
-export const defaultFocuses: Record<number, string> = {
-  0: "Sparring",
-  1: "Forms",
-  2: "Self-defense",
-  3: "Conditioning",
-  4: "Weapons training",
-};
-
